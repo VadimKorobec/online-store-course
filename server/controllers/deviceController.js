7b311b72cef232c8ebd4f5e9c31b4c096a5abc15
Vadim <_s_e_t__@ukr.net>
2025-05-07 17:36:08 +0300

    add some code

--- server/controllers/deviceController.js
+++ server/controllers/deviceController.js
@@ -1,4 +1,7 @@
 const uuid = require("uuid");
+const path = require("path");
+const { Device } = require("../models/models");
+const HttpError = require("../error/HttpError");
 
 exports.getAll = async (req, res, next) => {
   try {
@@ -10,8 +13,36 @@ exports.getAll = async (req, res, next) => {
 exports.create = async (req, res, next) => {
   try {
     const { name, price, brandId, typeId, info } = req.body;
+    const requiredFields = { name, price, brandId, typeId};
     const { img } = req.files;
-    let fileName = uuid.v4() + '.jpg'
+
+    const missingFields = Object.keys(requiredFields)
+      .filter((key) => !requiredFields[key])
+      .join(", ");
+
+    if (missingFields) {
+      throw HttpError(
+        400,
+        `Missing required fields: ${missingFields}`
+      );
+    }
+
+    if (!img) {
+      throw HttpError(400, "image file is required");
+    }
+
+    let fileName = uuid.v4() + ".jpg";
+    await img.mv(path.resolve(__dirname, "..", "static", fileName));
+
+    const device = await Device.create({
+      name,
+      price,
+      brandId,
+      typeId,
+      info,
+      img: fileName,
+    });
+    res.status(201).json({ device });
   } catch (error) {
     next(error);
   }
--- server/index.js
+++ server/index.js
@@ -2,7 +2,7 @@ require("dotenv").config();
 
 const express = require("express");
 const cors = require("cors");
-const fileUpload = require('express-fileupload')
+const fileUpload = require("express-fileupload");
 
 const sequelize = require("./db");
 const models = require("./models/models");
@@ -14,7 +14,8 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
-app.use(fileUpload({}))
+app.use(express.static());
+app.use(fileUpload({}));
 
 app.use("/api", apiRouter);
 
